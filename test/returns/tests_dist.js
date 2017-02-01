3a9111fc2ed52a9ce650b5e27488740f93235389
Roman Rubsamen <roman.rubsamen@gmail.com>
2017-02-01 16:01:21 +0100

    Added value at risk

--- test/returns/tests_dist.js
+++ test/returns/tests_dist.js
@@ -95,18 +95,39 @@ QUnit.test('Arithmetic returns computation', function(assert) {
 });
 
 
-QUnit.test('Gain to pain ratio incorrect input arguments', function(assert) {   
+QUnit.test('Value at risk incorrect input arguments', function(assert) {   
   assert.throws(function() {
-      PortfolioAnalytics.gainToPainRatio();
+      PortfolioAnalytics.valueAtRisk();
     },
-    new Error("input must be an array of positive numbers"),
+    new Error("input(s) must be a number"),
     "No input arguments"
   );
    
   assert.throws(function() {
-      PortfolioAnalytics.gainToPainRatio([-100]);
+      PortfolioAnalytics.valueAtRisk([-100]);
+    },
+    new Error ("input(s) must be a number"),
+    "Negative numeric array input argument"
+  );
+  
+  assert.throws(function() {
+      PortfolioAnalytics.valueAtRisk([-100], 1);
     },
     new Error ("input must be an array of positive numbers"),
+    "Negative numeric array input argument"
+  );
+  
+  assert.throws(function() {
+      PortfolioAnalytics.valueAtRisk([100], -0.01);
+    },
+    new Error ("input must be bounded between 0 and 1"),
+    "Negative numeric array input argument"
+  );
+  
+  assert.throws(function() {
+      PortfolioAnalytics.valueAtRisk([100], 1.01);
+    },
+    new Error ("input must be bounded between 0 and 1"),
     "Negative numeric array input argument"
   );
   
@@ -114,8 +135,12 @@ QUnit.test('Gain to pain ratio incorrect input arguments', function(assert) {
 });
 
 
-QUnit.test('Gain to pain ratio computation', function(assert) {    
-  assert.deepEqual(PortfolioAnalytics.gainToPainRatio([100]), NaN, 'Gain to pain ratio #1');
-  assert.equal(PortfolioAnalytics.gainToPainRatio([100, 100, 109.5, 108.405, 109.48905, 108.3941595, 107.8521887]), 3.1999999961252454, 'Gain to pain ratio #2');
-  assert.deepEqual(PortfolioAnalytics.gainToPainRatio([100, 110]), NaN, 'Gain to pain ratio #3'); 
+QUnit.test('Value at risk computation', function(assert) {    
+  assert.deepEqual(PortfolioAnalytics.valueAtRisk([100], 0.10), NaN, 'Value at Risk NaN #1');
+  
+  assert.equal(PortfolioAnalytics.valueAtRisk([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110], 0.10), -0.009174311926605505, 'Negative value at Risk');
+  
+  var aTestPortfolio = [100,93,88.815,87.0387,85.7331195,84.87578831,84.87578831,86.99768301,91.34756716,98.1986347,107.0365118];  
+  assert.equal(PortfolioAnalytics.valueAtRisk(aTestPortfolio, 0.10), 0.07, 'Value at Risk #1');
+  assert.deepEqual(PortfolioAnalytics.valueAtRisk(aTestPortfolio, 0.05), NaN, 'Value at Risk NaN #2');
 });
